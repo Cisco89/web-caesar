b0a8f322b15a293b7236dadbc11c57d9e078a70f
Cisco89 <franciscoadriangarcia1@gmail.com>
2019-06-20 20:35:38 -0600

    removed render_template and implemented the form vaiable

--- main.py
+++ main.py
@@ -5,9 +5,49 @@ import cgi
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
+form = """
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <style>
+            form {{
+                background-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-radius: 10px;
+            }}
+            textarea {{
+                margin: 10px 0;
+                width: 540px;
+                height: 120px;
+            }}
+        </style>
+    </head>
+    <body>
+        <form method="POST">
+
+            <label>Rotate by:
+                <input type="text" name="rot" value="0"/>
+            </label>
+
+            <textarea name="text">{0}</textarea>
+
+            <label></br>
+                <input type="submit" />
+                <input type="reset" />
+            </label>
+
+        </form>
+    </body>
+</html>
+"""
+
 @app.route("/")
 def index():
-    return render_template('my-form.html').format('')
+    return form.format('')
 
 @app.route("/", methods=['POST'])
 def encrypt():
@@ -17,6 +57,6 @@ def encrypt():
     encrypted_text = rotate_string(text, rot)
 
     # return '<h1>' + encrypted_text + '</h1>'
-    return render_template('my-form.html').format(encrypted_text)
+    return form.format(encrypted_text)
 
 app.run()
